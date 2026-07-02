4d99f8d29abafc1cdaced297fcc79700e610a10b
Yigit <ornek@mail.com>
2026-07-02 22:30:40 +0300

    Initial Commit

---
+++ PaperAI/PaperAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperAIApp.swift
+//  PaperAI
+//
+//  Created by yigitstrives on 2.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PaperAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaperAITests/PaperAITests.swift
@@ -0,0 +1,17 @@
+//
+//  PaperAITests.swift
+//  PaperAITests
+//
+//  Created by yigitstrives on 2.07.2026.
+//
+
+import Testing
+@testable import PaperAI
+
+struct PaperAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
